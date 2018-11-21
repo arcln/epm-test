f64594fb973fd86582e70ee6606546a7ba4f741c
Arthur Chaloin <arthur@achaloin.com>
2018-11-21 21:47:04 +0100

    test C project

---
+++ include/vector.h
@@ -0,0 +1,7 @@
+/**
+ * @author Arthur Chaloin
+ * Last modified  : 2018-11-21 21:42:36
+ */
+
+void vector_add();
+void vector_delete();
---
+++ src/vector.c
@@ -0,0 +1,16 @@
+/**
+ * @author Arthur Chaloin
+ * Last modified  : 2018-11-21 21:43:26
+ */
+
+#include <stdio.h>
+
+void vector_add()
+{
+  printf("add\n");
+}
+
+void vector_delete()
+{
+  printf("delete\n");
+}
